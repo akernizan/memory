23ed7bd153fd3acb99a323fb405ed17e25ea02d2
akosua kernizan <akernizan@gmail.com>
2015-07-03 16:56:54 -0400

    WIP what to do on click

--- javascripts/application.js
+++ javascripts/application.js
@@ -5,9 +5,39 @@ var yellowBox = document.getElementById('yellow');
 var greenBox = document.getElementById('green');
 var blueBox = document.getElementById('blue');
 
+var clicked = true;
+
 var init = function(){
 	$('#red').on('click', function(){
-		redBox.style.background = "rgba(255,0,0, 0.5)";
+		// var boxChange = setInterval(function(){
+			redBox.style.background = "rgba(255,0,0, 0.5)";
+			
+		// }, 500)
+		// clearInterval(boxChange);
+	});
+
+	$('#green').on('click', function(){
+		// var boxChange = setInterval(function(){
+			greenBox.style.background = "rgba(0,128,0,0.5)";
+			
+		// }, 500)
+		// clearInterval(boxChange);
+	});
+
+	$('#yellow').on('click', function(){
+		// var boxChange = setInterval(function(){
+			yellowBox.style.background = "rgba(255,255,0,0.5)";
+			
+		// }, 500)
+		// clearInterval(boxChange);
+	});
+
+		$('#blue').on('click', function(){
+		// var boxChange = setInterval(function(){
+			blueBox.style.background = "rgba(0,0,255,0.5)";
+			
+		// }, 500)
+		// clearInterval(boxChange);
 	});
 }
 
